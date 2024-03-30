88b50e84f878c8c0c7972e65c533f8daeb99810f
igoritos22 <igoritos_22@hotmail.com>
2024-03-30 18:07:59 -0300

    feat: add docs with swagger

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/igordevopslabs/jiujitsu-posts/config"
 	"github.com/igordevopslabs/jiujitsu-posts/controller"
+	docs "github.com/igordevopslabs/jiujitsu-posts/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func init() {
@@ -12,9 +15,18 @@ func init() {
 	config.ConnectToDB()
 }
 
+// @title API Jiu-Jitsu Posts
+// @version 1.0
+// @description API simples para retornar os maiores jargões do jiu-jitsu através de posts objetivos.
+// @host localhost:5000
+// @BasePath /
+
 func main() {
 	r := gin.Default()
 
+	docs.SwaggerInfo.BasePath = "/"
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	r.POST("/post", controller.CreatePost)
 	r.GET("/posts", controller.GetAllPosts)
 	r.GET("/posts/:id", controller.GetPostByID)
