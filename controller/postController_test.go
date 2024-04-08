f60e88baf22d50b0cfbcd2980dba8aa7f3b56bf6
igoritos22 <igoritos_22@hotmail.com>
2024-04-08 13:54:07 -0300

    feat: add testes to delete route

--- controller/postController_test.go
+++ controller/postController_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/gin-gonic/gin"
 	"github.com/igordevopslabs/jiujitsu-posts/config"
+	"github.com/igordevopslabs/jiujitsu-posts/middleware"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gorm.io/driver/postgres"
@@ -73,6 +74,7 @@ func TestGetPostID(t *testing.T) {
 	// Configura o SQL mock
 	// Pesquisar substituição correta dos placeholders
 	// Pesquiser os parametros suportados pelo WithArgs()
+
 	query := `SELECT \* FROM "posts" WHERE "posts"."id" = \$1 AND "posts"."deleted_at" IS NULL ORDER BY "posts"."id" LIMIT \$2`
 	mock.ExpectQuery(query).
 		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg()).
@@ -93,4 +95,34 @@ func TestGetPostID(t *testing.T) {
 	assert.Equal(t, 200, resp.Code)
 
 	//tentar fazer got pelo body da req
+}
+
+func TestDeletePost(t *testing.T) {
+	// Inicializa o Gin e coloca ele no modo de teste
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+
+	gormDB, mock := SetupMockDB(t) //invoca a função
+
+	// Substitui a config.DB global (ou similar) pelo GORM mock.
+	config.DB = gormDB
+
+	query := `UPDATE "posts" SET "deleted_at"=\$1 WHERE "posts"."id" = \$2 AND "posts"."deleted_at" IS NULL`
+	mock.ExpectBegin()
+	mock.ExpectExec(query).WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg()).WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	// Adiciona a rota ao router do Gin em teste mode
+	router.DELETE("/post/:id", middleware.RequireAuth, DeletePost)
+
+	// Cria uma requisição GET para /posts
+	req, _ := http.NewRequest("DELETE", "/post/113", nil)
+
+	resp := httptest.NewRecorder()
+
+	// Faz a requisição ao Gin
+	router.ServeHTTP(resp, req)
+
+	// Verifica se a resposta é a esperada
+	assert.Equal(t, 200, resp.Code)
 }
